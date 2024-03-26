bb6ce1232b4f5b9e57be8bc65586949534b1737e
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-27 00:02:22 +0300

    feat: adds new logic for  layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="any"
+          href="https://prismic.io/favicon.ico"
+        />
+      </head>
+      <body className="flex flex-col items-center bg-stone-50">
+        <div className="bg-white max-w-7xl min-h-screen border-x border-solid border-gray-200 p-12 w-full flex flex-col gap-20 items-center text-slate-700">
+          {children} <PrismicPreview repositoryName={repositoryName} />
+        </div>
+      </body>
     </html>
   );
 }
