b74f1e1b8ab9edbb459cfe8f0df7ecbcc66bf740
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-03-31 18:40:09 +0300

    feat: adds new component PostCard and new slice hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,7 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content } from '@prismicio/client';
+import { SliceComponentProps, PrismicText } from '@prismicio/react';
+import { RichText } from '@/components/RichText';
+import { PrismicNextImage } from '@prismicio/next';
 
 /**
  * Props for `Hero`.
@@ -12,10 +14,21 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
     <section
+      className="flex flex-col gap-4 max-w-3xl w-full"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
+      <PrismicNextImage
+        field={slice.primary.image}
+        sizes="100vw"
+        className="w-full max-w-[100px] max-h-full rounded-md object-cover"
+      />
+      <div className="flex flex-col gap-2">
+        <h1 className="font-bold text-4xl">
+          <PrismicText field={slice.primary.title} />
+        </h1>
+        <RichText field={slice.primary.description} />
+      </div>
     </section>
   );
 };
