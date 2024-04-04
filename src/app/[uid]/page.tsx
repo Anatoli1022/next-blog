86c164ba9c618409b023a60b51b26aa86cdb4a21
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-05 00:22:47 +0300

    feat: adds page id

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -1,3 +1,5 @@
+// ./src/app/blog/[uid]/page.tsx
+
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
@@ -6,6 +8,10 @@ import * as prismic from "@prismicio/client";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import { PrismicNextImage } from "@prismicio/next";
+import { PostCard } from "@/components/PostCard";
+import { RichText } from "@/components/RichText";
+import { Navigation } from "@/components/Navigation";
 
 type Params = { uid: string };
 
@@ -20,7 +26,7 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const client = createClient();
   const page = await client
-    .getByUID("page", params.uid)
+    .getByUID("blog_post", params.uid)
     .catch(() => notFound());
 
   return {
@@ -39,11 +45,70 @@ export async function generateMetadata({
 
 export default async function Page({ params }: { params: Params }) {
   const client = createClient();
+
+  // Fetch the current blog post page being displayed by the UID of the page
   const page = await client
-    .getByUID("page", params.uid)
+    .getByUID("blog_post", params.uid)
     .catch(() => notFound());
 
-  return <SliceZone slices={page.data.slices} components={components} />;
+  /**
+   * Fetch all of the blog posts in Prismic (max 2), excluding the current one, and ordered by publication date.
+   *
+   * We use this data to display our "recommended posts" section at the end of the blog post
+   */
+  const posts = await client.getAllByType("blog_post", {
+    predicates: [prismic.filter.not("my.blog_post.uid", params.uid)],
+    orderings: [
+      { field: "my.blog_post.publication_date", direction: "desc" },
+      { field: "document.first_publication_date", direction: "desc" },
+    ],
+    limit: 2,
+  });
+
+  // Destructure out the content of the current page
+  const { slices, title, publication_date, description, featured_image } =
+    page.data;
+
+  return (
+    <div className="flex flex-col gap-12 w-full max-w-3xl">
+      <Navigation client={client} />
+
+      {/* Display the "hero" section of the blog post */}
+      <section className="flex flex-col gap-12">
+        <div className="flex flex-col items-center gap-3 w-full">
+          <div className="flex flex-col gap-6 items-center">
+            <p className="opacity-75 border-b-2 w-min pb-1">
+              {new Date(publication_date || "").toLocaleDateString()}
+            </p>
+            <div className="text-center">
+              <RichText field={title} />
+            </div>
+          </div>
+          <div className="text-center">
+            <RichText field={description} />
+          </div>
+        </div>
+        <PrismicNextImage
+          field={featured_image}
+          sizes="100vw"
+          className="w-full max-w-3xl max-h-96 rounded-xl object-cover"
+        />
+      </section>
+
+      {/* Display the content of the blog post */}
+      <SliceZone slices={slices} components={components} />
+
+      {/* Display the Recommended Posts section using the posts we requested earlier */}
+      <h2 className="font-bold text-3xl">Recommended Posts</h2>
+      <section className="grid grid-cols-1 gap-8 max-w-3xl w-full">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </section>
+
+      <Navigation client={client} />
+    </div>
+  );
 }
 
 export async function generateStaticParams() {
@@ -52,9 +117,7 @@ export async function generateStaticParams() {
   /**
    * Query all Documents from the API, except the homepage.
    */
-  const pages = await client.getAllByType("page", {
-    predicates: [prismic.filter.not("my.page.uid", "home")],
-  });
+  const pages = await client.getAllByType("blog_post");
 
   /**
    * Define a path for every Document.
