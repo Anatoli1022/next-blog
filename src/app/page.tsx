8018cd39eb6cb2c4b001b7810927ca61619b51b1
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-06 23:44:47 +0300

    feat: adds main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,60 @@
-import { Metadata } from "next";
 
-import { SliceZone } from "@prismicio/react";
-import * as prismic from "@prismicio/client";
 
-import { createClient } from "@/prismicio";
-import { components } from "@/slices";
+import { Metadata } from 'next';
 
-// This component renders your homepage.
-//
-// Use Next's generateMetadata function to render page metadata.
-//
-// Use the SliceZone to render the content of the page.
+import { SliceZone } from '@prismicio/react';
+import * as prismic from '@prismicio/client';
+
+import { createClient } from '@/prismicio';
+import { components } from '@/slices';
+import { PostCard } from '@/components/PostCard';
+import { Navigation } from '@/components/Navigation';
+
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getByUID("page", "home");
+  const home = await client.getByUID('page', 'home');
 
   return {
     title: prismic.asText(home.data.title),
     description: home.data.meta_description,
     openGraph: {
-      title: home.data.meta_title ?? undefined,
-      images: [{ url: home.data.meta_image.url ?? "" }],
+      title: home.data.meta_title || undefined,
+      images: [
+        {
+          url: home.data.meta_image.url || '',
+        },
+      ],
     },
   };
 }
 
 export default async function Index() {
-  // The client queries content from the Prismic API
+  
   const client = createClient();
-  const home = await client.getByUID("page", "home");
 
-  return <SliceZone slices={home.data.slices} components={components} />;
+  
+  const home = await client.getByUID('page', 'home');
+
+  
+  const posts = await client.getAllByType('blog_post', {
+    orderings: [
+      { field: 'my.blog_post.publication_date', direction: 'desc' },
+      { field: 'document.first_publication_date', direction: 'desc' },
+    ],
+  });
+
+  return (
+    <>
+      <Navigation client={client} />
+
+      <SliceZone slices={home.data.slices} components={components} />
+
+      <section className="grid grid-cols-1 gap-8 max-w-3xl w-full">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </section>
+    </>
+  );
 }
