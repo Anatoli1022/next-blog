eac3b82b699a3bdf04284f637fd2844944bc19f0
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-17 19:34:05 +0300

    fix: fix route for supabase

---
+++ src/app/api/comments/route.ts
@@ -0,0 +1,29 @@
+
+import { supabase } from '@/lib/supabase/server';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: any) {
+  const body = await req.json();
+
+  const { post_id, email, comment, nickname, uid } = body;
+
+  const { data, error } = await supabase
+    .from('comments')
+    .insert({
+      post_id,
+      email,
+      nickname,
+      payload: comment,
+    })
+    .select('id');
+
+  if (error) {
+    return new NextResponse(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+
+  return new NextResponse(JSON.stringify({ success: true }), {
+    status: 200,
+  });
+}
