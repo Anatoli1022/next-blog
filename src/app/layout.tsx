1caf4698ae1946ca82bddc4668b68d2a06aa8925
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-29 23:10:06 +0300

    feat: adds navigation in mail layout and new styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { PrismicPreview } from '@prismicio/next';
 import { repositoryName } from '@/prismicio';
 import './styles.css';
+import { Navigation } from '@/components/Navigation';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,8 +17,9 @@ export default function RootLayout({
           href="https://prismic.io/favicon.ico"
         />
       </head>
-      <body className="flex flex-col items-center bg-stone-50">
-        <div className="bg-white max-w-7xl min-h-screen border-x border-solid border-gray-200 p-12 w-full flex flex-col gap-20 items-center text-slate-700">
+      <body className="max-w-screen-xl ml-auto mr-auto">
+        <Navigation />
+        <div className="bg-white max-w-7xl min-h-screen p-12 w-full flex flex-col gap-20 items-center text-slate-700">
           {children} <PrismicPreview repositoryName={repositoryName} />
         </div>
       </body>
