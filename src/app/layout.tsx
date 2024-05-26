88ac33ac2dc3ad838269a567d44c1e67c47d1bf0
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-05-26 17:16:13 +0300

    feat: new styles for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
           href="https://prismic.io/favicon.ico"
         />
       </head>
-      <body className="max-w-screen-xl ml-auto mr-auto">
+      <body className="max-w-screen-xl ml-auto mr-auto bg-gray-50">
         <Navigation />
-        <div className="bg-white max-w-7xl min-h-screen p-12 w-full flex flex-col gap-20 items-center text-slate-700">
+        <div className=" max-w-7xl min-h-screen p-12 w-full flex flex-col gap-20 items-center text-slate-700">
           {children} <PrismicPreview repositoryName={repositoryName} />
         </div>
       </body>
