b51d337f0c1eb463311f5be226dc89a72b90fa7b
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-06-11 20:24:39 +0300

    feat: adds page about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,43 @@
+
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
+import * as prismic from "@prismicio/client";
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const about = await client.getByUID("page", "about");
+
+  return {
+    title: prismic.asText(about.data.title),
+    description: about.data.meta_description,
+    openGraph: {
+      title: about.data.meta_title || undefined,
+      images: [
+        {
+          url: about.data.meta_image.url || "",
+        },
+      ],
+    },
+  };
+}
+
+export default async function Index() {
+  const client = createClient();
+
+  const about = await client.getByUID("page", "about", {
+    fetchOptions: {
+      next: { revalidate: 600 },
+    },
+  });
+
+
+  return (
+    <>
+      <SliceZone slices={about.data.slices} components={components} />
+
+    </>
+  );
+}
