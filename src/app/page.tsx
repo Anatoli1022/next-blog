75a9b0c6570b4290069b8012ce34aa160ae22655
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-06-11 20:24:03 +0300

    fix: fix main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { Metadata } from 'next';
-import { SliceZone } from '@prismicio/react';
-import * as prismic from '@prismicio/client';
-import { createClient } from '@/prismicio';
-import { components } from '@/slices';
-import { PostCard } from '@/components/PostCard';
-
+import { Metadata } from "next";
+import * as prismic from "@prismicio/client";
+import { createClient } from "@/prismicio";
+import PostList from "@/components/PostList";
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getByUID('page', 'home');
+  const home = await client.getByUID("page", "home");
 
   return {
     title: prismic.asText(home.data.title),
@@ -17,7 +14,7 @@ export async function generateMetadata(): Promise<Metadata> {
       title: home.data.meta_title || undefined,
       images: [
         {
-          url: home.data.meta_image.url || '',
+          url: home.data.meta_image.url || "",
         },
       ],
     },
@@ -27,33 +24,12 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Index() {
   const client = createClient();
 
-  const home = await client.getByUID('page', 'home', {
+  const home = await client.getByUID("page", "home", {
     fetchOptions: {
-      next: { revalidate: 0 },
+      next: { revalidate: 3600 },
+      // next: { revalidate: 0 },
     },
   });
 
-  const posts = await client.getAllByType('blog_post', {
-    orderings: [
-      { field: 'my.blog_post.publication_date', direction: 'desc' },
-      { field: 'document.first_publication_date', direction: 'desc' },
-    ],
-    fetchOptions: {
-      next: { revalidate: 0 },
-    },
-  });
-
-  return (
-    <>
-      
-
-      <SliceZone slices={home.data.slices} components={components} />
-
-      <section className="grid grid-cols-1 gap-8 max-w-3xl w-full">
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
-      </section>
-    </>
-  );
+  return <PostList />;
 }
