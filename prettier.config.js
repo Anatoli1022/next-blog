10d552c31521c9ba4b06959b7a2a3442a5b3511a
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-06-16 19:27:54 +0300

    fix: fix prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  plugins: [
-  
-    (async () => {
-      try {
-        const plugin = await import("prettier-plugin-tailwindcss");
-        return plugin.default || plugin; 
-      } catch (error) {
-        console.error('Failed to import prettier-plugin-tailwindcss:', error);
-        return null; 
-      }
-    })(),
-  ],
+  plugins: [require("prettier-plugin-tailwindcss")],
 };
