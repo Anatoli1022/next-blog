6305698568f61e0322e60f77ab22b13944dace71
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-06-17 00:11:54 +0300

    fix: fix reval

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -15,7 +15,7 @@ export function CommentForm({ id, uid, revalidate, user }: CommentsProps) {
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
-    revalidate(`https://next-blog-ruby-eight.vercel.app/${uid}`);
+    revalidate(`/${uid}`);
 
     if (user) {
       await fetch(`/api/comments`, {
