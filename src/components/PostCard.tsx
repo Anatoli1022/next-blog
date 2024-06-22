c463a662699b9bde6561ae69ad2edb12f4a23d6d
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-06-22 23:19:49 +0300

    fix: fix styles in PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -10,22 +10,22 @@ export const PostCard = ({ post }: { post: Content.BlogPostDocument }): JSX.Elem
   return (
     <PrismicNextLink
       document={post}
-      className='col-span-2 grid w-full grid-cols-2 gap-10 border-b border-solid border-gray-200 last:border-none'
+      className='col-span-2 grid w-full grid-cols-2 items-center gap-10 border-b border-solid border-gray-200 pb-3 last:border-none'
     >
       <PrismicNextImage
         field={data.featured_image}
         sizes='100vw'
         className='max-h-60 w-full max-w-sm rounded-xl object-cover'
-        width={364}
+        width={384}
         height={240}
         fallbackAlt=''
       />
       <div className='flex flex-col gap-3'>
-        <div className='flex flex-col gap-1'>
+        <div className='flex flex-col gap-2'>
           <p className='w-min border-b-2 pb-1 text-sm text-slate-700 opacity-75'>
             {new Date(data?.publication_date || "").toLocaleDateString()}
           </p>
-          <ul className='mt-2 flex flex-wrap gap-2'>
+          <ul className='flex flex-wrap gap-2'>
             {post.tags.map((item, i) => {
               return (
                 <li key={i} className='rounded-2xl bg-indigo-300 px-2 py-1 text-xs'>
@@ -34,11 +34,10 @@ export const PostCard = ({ post }: { post: Content.BlogPostDocument }): JSX.Elem
               );
             })}
           </ul>
-          <div className='transition-all duration-300 ease-in-out hover:opacity-75'>
-            <h2 className='text-xl font-bold'>
-              <PrismicText field={data.title} />
-            </h2>
-          </div>
+
+          <h2 className='text-2xl font-bold'>
+            <PrismicText field={data.title} />
+          </h2>
         </div>
         <RichText field={data.description} />
       </div>
