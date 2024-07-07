17b62b13e1ecb31cf4425210d033c04ad7b7cd8e
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-07 22:08:30 +0300

    feat: registration page

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -7,8 +7,8 @@ import { components } from "@/slices";
 import { headers } from "next/headers";
 import { createClientUser } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
-// import { cookies } from 'next/headers';
+import { SubmitButton } from "@/components/submit-button";
+
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
@@ -38,26 +38,6 @@ export default async function Index({ searchParams }: { searchParams: { message:
   });
 
   // cookies();
-  const signIn = async (formData: FormData) => {
-    "use server";
-
-    const supabase = createClientUser();
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
-
-    if (error) {
-      return redirect(
-        `/registration?message=${encodeURIComponent("Sign-in failed. Please check your credentials and try again.")}`,
-      );
-    }
-
-    return redirect("/");
-  };
 
   const signUp = async (formData: FormData) => {
     "use server";
@@ -66,12 +46,15 @@ export default async function Index({ searchParams }: { searchParams: { message:
 
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-
+    const nickname = formData.get("nickname") as string;
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
       options: {
         emailRedirectTo: `${origin}/auth/callback`,
+        data: {
+          nickname: nickname,
+        },
       },
     });
 
@@ -86,37 +69,39 @@ export default async function Index({ searchParams }: { searchParams: { message:
     <>
       <SliceZone slices={registration.data.slices} components={components} />
 
-      <div className='flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md'>
-        <form className='animate-in text-foreground flex w-full flex-1 flex-col justify-center gap-2'>
-          <label className='text-md' htmlFor='email'>
+      <div className='flex w-full flex-1 flex-col justify-center px-8 sm:max-w-md'>
+        <form className='animate-in text-foreground flex w-full flex-1 flex-col justify-center'>
+          <label className='text-md' htmlFor='nickname'>
+            Nickname
+          </label>
+          <input
+            className='mt-3 rounded-md border bg-inherit px-4 py-2'
+            name='nickname'
+            placeholder='Kabachok153'
+            required
+          />
+          <label className='text-md mt-6' htmlFor='email'>
             Email
           </label>
           <input
-            className='mb-6 rounded-md border bg-inherit px-4 py-2'
+            className='mt-3 rounded-md border bg-inherit px-4 py-2'
             name='email'
             placeholder='you@example.com'
             required
           />
-          <label className='text-md' htmlFor='password'>
+          <label className='text-md mt-6' htmlFor='password'>
             Password
           </label>
           <input
-            className='mb-6 rounded-md border bg-inherit px-4 py-2'
+            className='mt-3 rounded-md border bg-inherit px-4 py-2'
             type='password'
             name='password'
             placeholder='••••••••'
             required
-          />
-          <SubmitButton
-            formAction={signIn}
-            className='text-foreground mb-2 rounded-md border bg-indigo-400 px-4 py-2 text-white transition hover:bg-inherit hover:text-black'
-            pendingText='Signing In...'
-          >
-            Sign In
-          </SubmitButton>
+          />{" "}
           <SubmitButton
             formAction={signUp}
-            className='border-foreground/20 text-foreground mb-2 rounded-md border px-4 py-2 transition hover:bg-indigo-400 hover:text-white'
+            className='text-foreground mt-6 rounded-md border bg-indigo-400 px-4 py-2 text-white transition hover:bg-inherit hover:text-black'
             pendingText='Signing Up...'
           >
             Sign Up
