2c360904fe4ee70ef00ce302d821fa6a31318192
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-08 23:23:59 +0300

    feat: adds formik in RegistrationForm

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,14 +1,23 @@
 "use client";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import validationSchema from "@/schema/schema";
 import { useState } from "react";
 import { SubmitButton } from "./submit-button";
 import Image from "next/image";
 import imageShow from "@/assets/eye-password-show-svgrepo-com.svg";
 import imageHide from "@/assets/eye-password-hide-svgrepo-com.svg";
+interface SignUpFormData {
+  nickname: string;
+  email: string;
+  password: string;
+}
 
 interface RegistrationFormProps {
-  signUp: (formData: FormData) => Promise<void>;
-  searchParams: { message?: string };
+  signUp: (formData: SignUpFormData) => Promise<void>;
+  searchParams?: { message?: string };
 }
+
 const RegistrationForm = ({ signUp, searchParams }: RegistrationFormProps) => {
   const [showPassword, setShowPassword] = useState(false);
   const togglePasswordVisibility = () => {
@@ -16,49 +25,62 @@ const RegistrationForm = ({ signUp, searchParams }: RegistrationFormProps) => {
   };
 
   return (
-    <form className='animate-in text-foreground flex w-full flex-1 flex-col justify-center'>
-      <label className='text-md' htmlFor='nickname'>
-        Nickname
-      </label>
-      <input
-        className='mt-3 rounded-md border bg-inherit px-4 py-2'
-        name='nickname'
-        placeholder='Kabachok153'
-        required
-      />
-      <label className='text-md mt-6' htmlFor='email'>
-        Email
-      </label>
-      <input
-        className='mt-3 rounded-md border bg-inherit px-4 py-2'
-        name='email'
-        placeholder='you@example.com'
-        required
-      />
-      <label className='text-md mt-6' htmlFor='password'>
-        Password
-      </label>
-      <div className='relative'>
-        <input
-          className='mt-3 min-w-full rounded-md border bg-inherit px-4 py-2'
-          type={showPassword ? "text" : "password"}
-          name='password'
-          placeholder='••••••••'
-          required
+    <Formik
+      initialValues={{ nickname: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values: SignUpFormData, { setSubmitting }) => {
+        signUp(values).finally(() => {
+          setSubmitting(false);
+        });
+      }}
+    >
+      <Form className='animate-in text-foreground flex w-full flex-1 flex-col justify-center'>
+        <label className='text-md' htmlFor='nickname'>
+          Nickname
+        </label>
+        <Field className='mt-3 rounded-md border bg-inherit px-4 py-2' name='nickname' placeholder='Kabachok153' />
+        <ErrorMessage name='nickname' component='div' className='mt-1 text-red-600' />
+
+        <label className='text-md mt-6' htmlFor='email'>
+          Email
+        </label>
+        <Field
+          className='mt-3 rounded-md border bg-inherit px-4 py-2'
+          name='email'
+          placeholder='you@example.com'
+          type='email'
         />
-        <button type='button' className='absolute inset-y-5 right-2 w-6 opacity-80' onClick={togglePasswordVisibility}>
-          {showPassword ? <Image src={imageHide} alt='' /> : <Image src={imageShow} alt='' />}
-        </button>
-      </div>
-      <SubmitButton
-        formAction={signUp}
-        className='text-foreground mt-6 rounded-md border bg-indigo-400 px-4 py-2 text-white transition hover:bg-inherit hover:text-black'
-        pendingText='Signing Up...'
-      >
-        Sign Up
-      </SubmitButton>
-      {searchParams?.message && <p className='text-center text-red-600'>{searchParams.message}</p>}
-    </form>
+        <ErrorMessage name='email' component='div' className='mt-1 text-red-600' />
+
+        <label className='text-md mt-6' htmlFor='password'>
+          Password
+        </label>
+        <div className='relative'>
+          <Field
+            className='mt-3 min-w-full rounded-md border bg-inherit px-4 py-2'
+            type={showPassword ? "text" : "password"}
+            name='password'
+            placeholder='••••••••'
+          />
+          <button
+            type='button'
+            className='absolute inset-y-5 right-2 w-6 opacity-80'
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <Image src={imageHide} alt='' /> : <Image src={imageShow} alt='' />}
+          </button>
+        </div>
+        <ErrorMessage name='password' component='div' className='mt-1 text-red-600' />
+
+        <SubmitButton
+          className='text-foreground mt-6 rounded-md border bg-indigo-400 px-4 py-2 text-white transition hover:bg-inherit hover:text-black'
+          pendingText='Signing Up...'
+        >
+          Sign Up
+        </SubmitButton>
+        {searchParams?.message && <p className='text-center text-red-600'>{searchParams.message}</p>}
+      </Form>
+    </Formik>
   );
 };
 
