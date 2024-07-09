29f6aff61a21ef61799fe5074907a2bff26a9aa8
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-09 23:55:04 +0300

    feat: create new logic for form registration

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -34,52 +34,65 @@ const RegistrationForm = ({ signUp, searchParams }: RegistrationFormProps) => {
         });
       }}
     >
-      <Form className='animate-in text-foreground flex w-full flex-1 flex-col justify-center'>
-        <label className='text-md' htmlFor='nickname'>
-          Nickname
-        </label>
-        <Field className='mt-3 rounded-md border bg-inherit px-4 py-2' name='nickname' placeholder='Kabachok153' />
-        <ErrorMessage name='nickname' component='div' className='mt-1 text-red-600' />
-
-        <label className='text-md mt-6' htmlFor='email'>
-          Email
-        </label>
-        <Field
-          className='mt-3 rounded-md border bg-inherit px-4 py-2'
-          name='email'
-          placeholder='you@example.com'
-          type='email'
-        />
-        <ErrorMessage name='email' component='div' className='mt-1 text-red-600' />
-
-        <label className='text-md mt-6' htmlFor='password'>
-          Password
-        </label>
-        <div className='relative'>
+      {({ isSubmitting }) => (
+        <Form className='animate-in text-foreground flex w-full flex-1 flex-col justify-center'>
+          <label className='text-md' htmlFor='nickname'>
+            Nickname
+          </label>
           <Field
-            className='mt-3 min-w-full rounded-md border bg-inherit px-4 py-2'
-            type={showPassword ? "text" : "password"}
-            name='password'
-            placeholder='••••••••'
+            className='mt-3 rounded-md border bg-inherit px-4 py-2'
+            name='nickname'
+            autoComplete='nickname'
+            id='nickname'
+            placeholder='Kabachok153'
           />
-          <button
-            type='button'
-            className='absolute inset-y-5 right-2 w-6 opacity-80'
-            onClick={togglePasswordVisibility}
-          >
-            {showPassword ? <Image src={imageHide} alt='' /> : <Image src={imageShow} alt='' />}
-          </button>
-        </div>
-        <ErrorMessage name='password' component='div' className='mt-1 text-red-600' />
+          <ErrorMessage name='nickname' component='div' className='mt-1 text-red-600' />
 
-        <SubmitButton
-          className='text-foreground mt-6 rounded-md border bg-indigo-400 px-4 py-2 text-white transition hover:bg-inherit hover:text-black'
-          pendingText='Signing Up...'
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && <p className='text-center text-red-600'>{searchParams.message}</p>}
-      </Form>
+          <label className='text-md mt-5' htmlFor='email'>
+            Email
+          </label>
+          <Field
+            className='mt-3 rounded-md border bg-inherit px-4 py-2'
+            name='email'
+            autoComplete='email'
+            id='email'
+            placeholder='you@example.com'
+            type='email'
+          />
+          <ErrorMessage name='email' component='div' className='mt-1 text-red-600' />
+
+          <div className='relative mt-5'>
+            <label className='text-md' htmlFor='password'>
+              Password
+            </label>
+            <Field
+              className='mt-3 min-w-full rounded-md border bg-inherit px-4 py-2'
+              type={showPassword ? "text" : "password"}
+              autoComplete='password'
+              id='password'
+              name='password'
+              placeholder='••••••••'
+            />
+            <button
+              type='button'
+              className='absolute inset-y-11 right-3 w-6 opacity-70'
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <Image src={imageHide} alt='' /> : <Image src={imageShow} alt='' />}
+            </button>
+          </div>
+          <ErrorMessage name='password' component='div' className='mt-1 text-red-600' />
+
+          <SubmitButton
+            className='text-foreground mt-6 rounded-md border bg-indigo-400 px-4 py-2 text-white transition hover:bg-inherit hover:text-black'
+            pendingText='Signing Up...'
+            isSubmitting={isSubmitting}
+          >
+            Sign Up
+          </SubmitButton>
+          {searchParams?.message && <p className='text-center text-red-600'>{searchParams.message}</p>}
+        </Form>
+      )}
     </Formik>
   );
 };
