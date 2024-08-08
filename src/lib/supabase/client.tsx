e25a70937a526e5e64271e1b43f3dc9923e71f5b
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-08-08 23:49:14 +0300

    feat: text supabase-client comments

--- src/lib/supabase/client.tsx
+++ src/lib/supabase/client.tsx
@@ -1,24 +1,6 @@
-//can be used instead of createBrowserClient
-// import { createClient } from "@supabase/supabase-js";
-import { createBrowserClient } from '@supabase/ssr'
-
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_COMMENTS || '';
-// const supabaseKey = process.env.SUPABASE_SERVICE_KEY_COMMENTS || '';
-
-// const supabaseComments = createClient(supabaseUrl, supabaseKey);
-
-// export { supabaseComments };
-
-// import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_COMMENTS || "";
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY_COMMENTS || "";
 
-export const createClientComments = () => createBrowserClient (supabaseUrl, supabaseKey);
-
-// const supabaseUrlUser = process.env.NEXT_PUBLIC_SUPABASE_URL_USERS || '';
-// const supabaseKeyUser = process.env.SUPABASE_SERVICE_KEY_USERS || '';
-
-// export const createClientUser = () =>
-//   createBrowserClient(supabaseUrlUser, supabaseKeyUser);
+export const createClientComments = () => createClient(supabaseUrl, supabaseKey);
