72a71d0c2edfd3243a61b6c3932dfe2ff8949ce1
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-08-26 02:20:32 +0300

    feat: image preaload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,13 @@ export default function RootLayout({
     <html lang='en'>
       <head>
         <link rel='icon' type='image/png' sizes='any' href='https://prismic.io/favicon.ico' />
+        <link
+          rel='preload'
+          as='image'
+          href='https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=3840'
+          imageSizes='100vw'
+          imageSrcSet='https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=640 640w, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=750 750w, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=828 828 Вт, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=1080 1080 Вт, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=1200 1200 Вт, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=1920 1920w, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=2048 2048w, https://images.prismic.io/next-webblog/Zm8KbZm069VX1yQK_midas-min.jpg?auto=format%2Ccompress&fit=max&w=3840 3840w'
+        />
       </head>
       <body className='ml-auto mr-auto max-w-7xl bg-gray-50'>
         <Navigation />
