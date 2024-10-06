3c1feb1e83203e2c84c4790d5d8406291407080a
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-10-06 17:50:41 +0300

    feat: new info for about

--- src/slices/About/index.tsx
+++ src/slices/About/index.tsx
@@ -1,6 +1,7 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { RichText } from "@/components/RichText";
+import { PrismicText } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 /**
  * Props for `About`.
@@ -14,8 +15,17 @@ const About = ({ slice }: AboutProps): JSX.Element => {
   return (
     <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
       <div className='text-center'>
-        <RichText field={slice.primary.main_title} />
-        <div className='mt-4'>
+        <h1 className='text-7xl font-bold'>
+          <PrismicText field={slice.primary.main_title} />
+        </h1>
+        <h2 className='mt-2 text-4xl font-semibold'>
+          <PrismicText field={slice.primary.title} />
+        </h2>
+        <div className='m-auto mt-2 w-full max-w-[280px]'>
+          <span className='block h-[2px] w-full bg-indigo-400'></span>
+          <span className='m-auto mt-1 block h-[2px] max-w-[200px] bg-indigo-400'></span>
+        </div>
+        <div className='mt-6'>
           <RichText field={slice.primary.text} />
         </div>
       </div>
@@ -34,18 +44,5 @@ const About = ({ slice }: AboutProps): JSX.Element => {
     </section>
   );
 };
-
-// navigation.data.list.map((item, i) => {
-//   return (
-//     <li key={i}>
-//       <PrismicNextLink
-//         field={item.item_link}
-//         className='text-lg font-normal text-black transition hover:text-indigo-400'
-//       >
-//         <PrismicNextImage field={item.item_image} width={32} height={32} />
-//       </PrismicNextLink>
-//     </li>
-//   );
-// })}
 
 export default About;
